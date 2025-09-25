4f3a316bcdb9d1ddd3f06cf9464ec54a75f7b057
goncaslo <olivsalo13090505@gmail.com>
2025-09-25 13:40:59 +0100

    logo images

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,9 +4,10 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'HallyuChat - Comunidade K-Pop',
   description: 'Conecte-se com fãs de K-Pop, notícias e chat em tempo real',
+  icons: '/icon-s.png'
 }
 
 export default function RootLayout({
